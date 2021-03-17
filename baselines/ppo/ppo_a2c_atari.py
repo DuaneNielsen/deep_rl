65b520a58ebe3561bf091631d6ef4f08e1a9633d
duanenielsen <duane.nielsen.rocks@gmail.com>
2021-03-17 09:28:32 -0700

    PPO Atari plays breakout!

--- baselines/ppo/ppo_a2c_atari.py
+++ baselines/ppo/ppo_a2c_atari.py
@@ -20,6 +20,8 @@ import checkpoint
 import baselines.helper as helper
 import numpy as np
 import os
+import capture
+import random
 
 if __name__ == '__main__':
 
@@ -37,7 +39,8 @@ if __name__ == '__main__':
     """ main loop control """
     parser.add_argument('--max_steps', type=int, default=100000000)
     parser.add_argument('--test_steps', type=int, default=3000000)
-    parser.add_argument('--test_episodes', type=int, default=5)
+    parser.add_argument('--test_episodes', type=int, default=10)
+    parser.add_argument('--capture_freq', type=int, default=5000)
 
     """ resume settings """
     parser.add_argument('--demo', action='store_true', default=False)
@@ -49,9 +52,10 @@ if __name__ == '__main__':
 
     """ hyper-parameters """
     parser.add_argument('--optim_lr', type=float, default=1e-4)
-    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--batch_size', type=int, default=512)
     parser.add_argument('--discount', type=float, default=0.99)
-    parser.add_argument('--hidden_dim', type=int, default=16)
+    parser.add_argument('--hidden_dim', type=int, default=64)
+    parser.add_argument('--exploration_noise', type=float, default=0.3)
 
     config = parser.parse_args()
 
@@ -70,14 +74,18 @@ if __name__ == '__main__':
     """ environment """
     def make_env():
         env = gym.make(config.env_name)
+        env = capture.VideoCapture(env, config.run_dir, freq=config.capture_freq)
         env = wrappers.EpisodicLifeEnv(env)
-        env = wrappers.NoopResetEnv(env)
+        if 'NOOP' in env.unwrapped.get_action_meanings():
+            env = wrappers.NoopResetEnv(env)
         if 'FIRE' in env.unwrapped.get_action_meanings():
             env = wrappers.FireResetEnv(env)
         env = wrappers.ClipState2D(env, 0, 24, 210 - 24, 160)
         env = wrappers.WarpFrame(env)
+        env = wrappers.ScaledFloatFrame(env)
         env = wrappers.Gradient(env)
         env = wrappers.ClipRewardEnv(env)
+        env = wrappers.PenalizeOneIfDone(env)
         #env = RemapActions(env, remap=np.array([1, 2, 3]))
 
         #env = LiveMonitor(env)
@@ -91,15 +99,17 @@ if __name__ == '__main__':
     train_env, train_buffer = bf.wrap(make_env())
     train_env = wandb_utils.LogRewards(train_env)
     if not config.silent:
-        train_env = Plot(train_env, episodes_per_point=20, title=f'Train ppo-a2c-{config.env_name}',
+        train_env = Plot(train_env, episodes_per_point=20, title=f'Train ppo-a2c-{config.env_name}-{config.run_id}',
                          history_length=200, refresh_cooldown=5.0)
 
     """ test env """
     test_env = make_env()
     if not config.silent:
-        test_env = Plot(test_env, episodes_per_point=config.test_episodes, title=f'Test ppo-a2c-{config.env_name}',
+        test_env = Plot(test_env, episodes_per_point=config.test_episodes, title=f'Test ppo-a2c-{config.env_name}-{config.run_id}',
                         history_length=200, refresh_cooldown=5.0)
     evaluator = helper.Evaluator(test_env)
+
+    actions = train_env.action_space.n
 
     """ network """
     class A2CNet(nn.Module):
@@ -112,56 +122,59 @@ if __name__ == '__main__':
             self.actions = actions
 
             self.conv1 = nn.Sequential(
-                nn.Conv2d(2, 16, kernel_size=3, stride=1, padding=1),
-                nn.SELU(inplace=True),
-                nn.MaxPool2d(kernel_size=2, stride=2))
+                nn.Conv2d(2, 32, kernel_size=3, stride=2, padding=1),
+                nn.SELU(inplace=True))
             self.conv2 = nn.Sequential(
-                nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
-                nn.SELU(inplace=True),
-                nn.MaxPool2d(kernel_size=2, stride=2))
+                nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1),
+                nn.SELU(inplace=True))
             self.conv3 = nn.Sequential(
-                nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-                nn.SELU(inplace=True),
-                nn.MaxPool2d(kernel_size=2, stride=2))
-            self.conv4 = nn.Sequential(
-                nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-                nn.SELU(inplace=True),
-                nn.MaxPool2d(kernel_size=2, stride=2))
-            self.conv5 = nn.Sequential(
-                nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-                nn.SELU(inplace=True),
-                nn.MaxPool2d(kernel_size=2, stride=2))
-            self.conv6 = nn.Sequential(
-                nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
-                nn.SELU(inplace=True),
-                nn.MaxPool2d(kernel_size=2, stride=2))
+                nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1),
+                nn.SELU(inplace=True))
+            # self.conv4 = nn.Sequential(
+            #     nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            #     nn.SELU(inplace=True),
+            #     nn.MaxPool2d(kernel_size=2, stride=2))
+            # self.conv5 = nn.Sequential(
+            #     nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            #     nn.SELU(inplace=True),
+            #     nn.MaxPool2d(kernel_size=2, stride=2))
+            # self.conv6 = nn.Sequential(
+            #     nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            #     nn.SELU(inplace=True),
+            #     nn.MaxPool2d(kernel_size=2, stride=2))
 
 
-            self.mlp = nn.Sequential(nn.Linear(512, hidden_dims), nn.SELU(inplace=True),
-                                     nn.Linear(hidden_dims, hidden_dims), nn.SELU(inplace=True),
-                                     nn.Linear(hidden_dims, actions+1))
+            # self.mlp = nn.Sequential(nn.Linear(512, hidden_dims), nn.SELU(inplace=True),
+            #                          nn.Linear(hidden_dims, hidden_dims), nn.SELU(inplace=True),
+            #                          nn.Linear(hidden_dims, actions+1))
+
+            #self.lstm = nn.LSTMCell(32 * 5 * 5, hidden_dims)
+            self.contract = nn.Sequential(nn.Linear(3872, hidden_dims), nn.SELU(inplace=True),
+                                          nn.Linear(hidden_dims, hidden_dims), nn.SELU(inplace=True))
+            self.value = nn.Linear(hidden_dims, 1)
+            self.action = nn.Linear(hidden_dims, actions)
 
         def forward(self, state):
             l1 = self.conv1(state.permute(0, 3, 1, 2))
             l2 = self.conv2(l1)
             l3 = self.conv3(l2)
-            l4 = self.conv4(l3)
-            l5 = self.conv5(l4)
-            l6 = self.conv6(l5)
+            # l4 = self.conv4(l3)
+            # l5 = self.conv5(l4)
+            # l6 = self.conv6(l5)
 
-            output = self.mlp(l6.flatten(start_dim=1))
-
-            value = output[..., 0:1]
-            actions = softmax(output[..., 1:self.actions+1], dim=1)
-            #actions = torch.cat([actions, 1 - actions], dim=1)
-            a_dist = Categorical(actions)
+            hidden = self.contract(l3.flatten(start_dim=1))
+            value = self.value(hidden)
+            action = softmax(self.action(hidden), dim=1)
+            noise = softmax(torch.ones_like(action), dim=1)
+            action = (1 - config.exploration_noise) * action + config.exploration_noise * noise
+            a_dist = Categorical(action)
             return value, a_dist
 
     a2c_net = A2CNet(
         input_dims=test_env.observation_space.shape[0],
         hidden_dims=config.hidden_dim,
-        actions=4)
-    optim = torch.optim.Adam(a2c_net.parameters(), lr=config.optim_lr)
+        actions=actions)
+    optim = torch.optim.Adam(a2c_net.parameters(), lr=config.optim_lr, weight_decay=0.0)
 
     a2c_net = ppo.PPOWrapModel(a2c_net).to(config.device)
 
