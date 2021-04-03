bd4f344b6020ed18e171d5d4df835f264f265970
duanenielsen <duane.nielsen.rocks@gmail.com>
2021-04-02 18:37:39 -0700

    Hacked the keyboard agent to record episodes.

--- env/keyboard_agent.py
+++ env/keyboard_agent.py
@@ -3,6 +3,9 @@ import sys, gym, time
 import numpy as np
 import env.wrappers as wrappers
 import matplotlib.pyplot as plt
+from argparse import ArgumentParser
+import buffer as bf
+import cv2
 
 #
 # Test yourself as a learning agent! Pass environment name as a command-line argument, for example:
@@ -12,51 +15,49 @@ import matplotlib.pyplot as plt
 
 #env = gym.make('BreakoutDeterministic-v4' if len(sys.argv)<2 else sys.argv[1])
 
+""" configuration """
+parser = ArgumentParser(description='configuration switches')
 
-class Gradient(gym.Wrapper):
-    def __init__(self, env):
-        super().__init__(env)
-        self.prev = None
+parser.add_argument('--save_buffer', type=str, default='buffer.pkl')
 
-    def reset(self):
-        state = self.env.reset()
-        self.prev = state
-        grad = state - self.prev
-        return np.concatenate((state, grad), axis=2)
+""" environment """
+parser.add_argument('--env_name', type=str, default='BreakoutDeterministic-v4')
+parser.add_argument('--env_render', action='store_true', default=False)
+parser.add_argument('--env_reward_scale', type=float, default=1.0)
+parser.add_argument('--env_reward_bias', type=float, default=0.0)
+parser.add_argument('--env_timelimit', type=int, default=3000)
+config = parser.parse_args()
 
-    def step(self, action):
-        state, reward, done, info = self.env.step(action)
-        grad = state - self.prev
-        self.prev = state
 
-        return np.concatenate((state, grad), axis=2), reward, done, info
+def rescale_reward(reward):
+    return reward * config.env_reward_scale - config.env_reward_bias
 
 
 def make_env():
-    env = gym.make('BreakoutDeterministic-v4')
+    env = gym.make('SpaceInvadersDeterministic-v4')
+    env = wrappers.TimeLimit(env.unwrapped, max_episode_steps=config.env_timelimit)
     env = wrappers.EpisodicLifeEnv(env)
     env = wrappers.NoopResetEnv(env)
     if 'FIRE' in env.unwrapped.get_action_meanings():
         env = wrappers.FireResetEnv(env)
     env = wrappers.ClipState2D(env, 0, 24, 210 - 24, 160)
     env = wrappers.WarpFrame(env)
-    env = Gradient(env)
+    env = wrappers.Gradient(env)
     env = wrappers.ClipRewardEnv(env)
     env = wrappers.PenalizeOneIfDone(env)
-
-    # env = RemapActions(env, remap=np.array([1, 2, 3]))
-
-    # env = LiveMonitor(env)
+    env = gym.wrappers.TransformReward(env, rescale_reward)
     return env
 
-plt.ion()
-fig = plt.figure()
-spec = plt.GridSpec(nrows=1, ncols=2, figure=fig)
 
-ax_state = fig.add_subplot(fig.add_subplot(spec[0, 0]))
-ax_grad = fig.add_subplot(fig.add_subplot(spec[0, 1]))
+# plt.ion()
+# fig = plt.figure()
+# spec = plt.GridSpec(nrows=1, ncols=2, figure=fig)
+#
+# ax_state = fig.add_subplot(fig.add_subplot(spec[0, 0]))
+# ax_grad = fig.add_subplot(fig.add_subplot(spec[0, 1]))
 
 env = make_env()
+env, buffer = bf.wrap(env)
 
 if not hasattr(env.action_space, 'n'):
     raise Exception('Keyboard agent only supports discrete action spaces')
@@ -83,7 +84,7 @@ def keymap(key):
 def key_press(key, mod):
     global human_agent_action, human_wants_restart, human_sets_pause
     if key==0xff0d: human_wants_restart = True
-    if key==32: human_sets_pause = not human_sets_pause
+    #if key==32: human_sets_pause = not human_sets_pause
     a = keymap(key)
     if a <= 0 or a >= ACTIONS: return
     human_agent_action = a
@@ -101,12 +102,12 @@ env.unwrapped.viewer.window.on_key_press = key_press
 env.unwrapped.viewer.window.on_key_release = key_release
 
 
-def plot(obser):
-    ax_state.clear()
-    ax_grad.clear()
-    ax_state.imshow(obser[:, :, 0])
-    ax_grad.imshow(obser[:, :, 1])
-    fig.canvas.draw()
+# `def plot(obser):
+#     ax_state.clear()
+#     ax_grad.clear()
+#     ax_state.imshow(obser[:, :, 0])
+#     ax_grad.imshow(obser[:, :, 1])
+#     fig.canvas.draw()`
 
 
 def rollout(env):
@@ -114,32 +115,20 @@ def rollout(env):
     human_wants_restart = False
     obser = env.reset()
     print('RESET')
-    plot(obser)
-    skip = 0
     total_reward = 0
     total_timesteps = 0
     while 1:
-        if not skip:
-            #print("taking action {}".format(human_agent_action))
-            a = human_agent_action
-            total_timesteps += 1
-            skip = SKIP_CONTROL
-        else:
-            skip -= 1
+        a = human_agent_action
 
         obser, r, done, info = env.step(a)
-        plot(obser)
+        total_timesteps += 1
         if r != 0:
             print("reward %0.3f" % r)
         total_reward += r
         window_still_open = env.render()
         if window_still_open==False: return False
-        if done: break
+        if done: print('DONE'); break
         if human_wants_restart: break
-        while human_sets_pause:
-            print('PAUSED')
-            env.render()
-            time.sleep(0.1)
         time.sleep(0.1)
     print("timesteps %i reward %0.2f" % (total_timesteps, total_reward))
 
@@ -147,6 +136,10 @@ print("ACTIONS={}".format(ACTIONS))
 print("Press keys 1 2 3 ... to take actions 1 2 3 ...")
 print("No keys pressed is taking action 0")
 
-while 1:
+for i in range(10):
     window_still_open = rollout(env)
     if window_still_open==False: break
+
+print(f'buffer length {len(buffer)}')
+
+bf.save(buffer, 'space_invaders.pkl')
