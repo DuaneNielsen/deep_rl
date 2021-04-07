1315c2d1b07341a940417e0c0a683f19d5d05c02
duanenielsen <duane.nielsen.rocks@gmail.com>
2021-04-06 17:12:03 -0700

    Soft Actor Critic v0.1

---
+++ algos/sac.py
@@ -0,0 +1,42 @@
+import torch
+from torch.nn.functional import mse_loss
+
+
+# todo add double q network
+
+
+def train(dl, q, policy, q_optim, policy_optim,
+          discount=0.99, polyak=0.095, alpha=0.2,
+          device='cpu', precision=torch.float32):
+
+    for s, a, s_p, r, d in dl:
+        s = s.type(precision).to(device)
+        a = a.to(device)
+        s_p = s_p.type(precision).to(device)
+        r = r.type(precision).to(device).unsqueeze(1)
+        d = (1.0 * ~d.to(device)).unsqueeze(1)
+
+        with torch.no_grad():
+            a_p_dist = policy(s_p)
+            a_p = a_p_dist.rsample()
+            y = r + d * discount * (q(s_p, a_p) - alpha * a_p_dist.log_prob(a_p))
+
+        ql = mse_loss(q(s, a), y)
+
+        q_optim.zero_grad()
+        ql.backward()
+        q_optim.step()
+
+        a_dist = policy(s)
+        a_ = a_dist.rsample()
+        pl = - torch.mean(q(s, a_) - alpha * a_dist.log_prob(a_))
+
+        policy_optim.zero_grad()
+        pl.backward()
+        policy_optim.step()
+
+        # todo add target q networks and polyak update
+        # if time_to_update_targets:
+        #     update target networks
+
+        break
