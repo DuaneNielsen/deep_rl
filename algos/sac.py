c03ba9265ae0e3c4c453d5ad8ee482cf3fbc4031
duanenielsen <duane.nielsen.rocks@gmail.com>
2021-04-09 13:09:56 -0700

    Discrete version of SAC

--- algos/sac.py
+++ algos/sac.py
@@ -2,9 +2,6 @@ import torch
 from torch.nn.functional import mse_loss
 
 
-# todo add double q network
-
-
 def train(dl, q, target_q, policy, q_optim, policy_optim,
           discount=0.99, polyak=0.095, alpha=0.2,
           device='cpu', precision=torch.float32):
@@ -30,6 +27,44 @@ def train(dl, q, target_q, policy, q_optim, policy_optim,
         a_dist = policy(s)
         a_ = a_dist.rsample()
         pl = - torch.mean(q(s, a_) - alpha * a_dist.log_prob(a_).sum(1, keepdim=True))
+
+        policy_optim.zero_grad()
+        pl.backward()
+        policy_optim.step()
+
+        for q_param, target_q_param in zip(q.parameters(), target_q.parameters()):
+            target_q_param.data.copy_(polyak * q_param.data + (1.0 - polyak) * target_q_param.data)
+
+        break
+
+
+def train_discrete(dl, q, target_q, policy, q_optim, policy_optim,
+          discount=0.99, polyak=0.095, alpha=0.2,
+          device='cpu', precision=torch.float32):
+
+    for s, a, s_p, r, d in dl:
+        s = s.type(precision).to(device)
+        a = a.to(device)
+        s_p = s_p.type(precision).to(device)
+        r = r.type(precision).to(device).unsqueeze(1)
+        d = (1.0 * ~d.to(device)).unsqueeze(1)
+
+        N = s.size(0)
+
+        with torch.no_grad():
+            a_p_dist = policy(s_p)
+            a_p = a_p_dist.rsample()
+            y = r + d * discount * (torch.sum(target_q(s_p) * a_p, dim=1, keepdim=True) - alpha * a_p_dist.log_prob(a_p).unsqueeze(1))
+
+        ql = mse_loss(q(s)[torch.arange(N), a].unsqueeze(1), y)
+
+        a_dist = policy(s)
+        a_ = a_dist.rsample()
+        pl = - torch.mean(torch.sum(q(s).detach() * a_, dim=1, keepdim=True) - alpha * a_dist.log_prob(a_))
+
+        q_optim.zero_grad()
+        ql.backward()
+        q_optim.step()
 
         policy_optim.zero_grad()
         pl.backward()
