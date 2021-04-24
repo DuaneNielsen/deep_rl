15d436118f5a853733ccb61c5b44a7ed753ef946
duanenielsen <duane.nielsen.rocks@gmail.com>
2021-04-24 16:08:05 -0700

    Added cql_alpha by dual-gradient descent

--- algos/cql.py
+++ algos/cql.py
@@ -2,9 +2,9 @@ import torch
 from torch.nn.functional import mse_loss
 
 
-def train_continuous(dl, q, target_q, policy, q_optim, policy_optim,
+def train_continuous(dl, q, target_q, policy, q_optim, policy_optim, cql_alpha_log, cql_alpha_optim,
                      sample_actions=8, amin=-1.0, amax=1.0,
-                     discount=0.99, polyak=0.095, q_update_ratio=2, policy_alpha=0.2, cql_alpha=1.0,
+                     discount=0.99, polyak=0.095, q_update_ratio=2, policy_alpha=0.2, lagrange_threshold=0.0,
                      device='cpu', precision=torch.float32
                      ):
     q_update = 1
@@ -26,6 +26,9 @@ def train_continuous(dl, q, target_q, policy, q_optim, policy_optim,
             a_p = a_p_dist.rsample()
             y = r + d * discount * target_q(s_p, a_p)
 
+            """ 
+            sample actions for logsumexp(Q(s, a)), this will find the soft maximum of Q(s, a)
+            """
             a_sample = []
             a_sample += [torch.empty(sample_actions, N, A, device=device).uniform_(amin, amax)]
             a_sample += [a_dist.rsample((sample_actions, ))]
@@ -33,9 +36,18 @@ def train_continuous(dl, q, target_q, policy, q_optim, policy_optim,
             a_sample = torch.stack(a_sample, dim=0).reshape(sample_actions * 3 * N, A)
             s_sample = s.view(1, N, S)[torch.zeros(sample_actions * 3, dtype=torch.long), :, :].reshape(sample_actions * 3 * N, S)
 
+        """ cql loss will be large when the Qmax of sampled actions is bigger than the Q value of the actions
+         actually observed in the dataset """
         q_sample = q(s_sample, a_sample).reshape(sample_actions * 3, N, 1, -1)
         q_replay = q(s, a)
         cql_loss = torch.logsumexp(q_sample, dim=0) - q_replay.detach()
+
+        """ tune cql_alpha by dual gradient decent (tune so cql_alpha is bigger when cql loss is bigger) """
+        cql_alpha = cql_alpha_log.exp().clamp(min=0.0, max=1e6)
+        cql_alpha_loss = - torch.mean(cql_alpha * (cql_loss - lagrange_threshold))
+        cql_alpha_optim.zero_grad()
+        cql_alpha_loss.backward(retain_graph=True)
+        cql_alpha_optim.step()
 
         qloss = torch.mean((q(s, a) - y) ** 2 / 2 + cql_alpha * cql_loss)
 
